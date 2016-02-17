5fb16557e0eee206cc5bd43566ebf3a4c5f48689
K. Oh <ohkay@khtdr.com>
2016-02-16 20:15:55 -0700

    compiling appears to work

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-cd ${0%'run-tests.sh'}
+cd ${0%'test.sh'}
 ln -sf pageboy "pageboy-test"
 ./pageboy-test -h
 export PATH=$PATH:$(pwd)
-for t in ./tests/*.sh; do
+for t in ./scripts/test-*.sh; do
   echo -n "$t ... "
   read -r expected <<< "$(tail -1 $t | sed 's/^#//')"
+  echo "$($t -c)" > $t.compiled
+  chmod +x $t.compiled
   read -r actual <<< "$($t)"
+  read -r compiled <<< "$($t.compiled)"
+  rm $t.compiled
   if [[ "$expected" == "$actual" ]]; then
     echo "passed"
   else
